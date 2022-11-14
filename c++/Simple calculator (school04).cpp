976ed5f9373f52af9e40a4fb8e8919ab8ec12cb0
Bruno S <81621691+srcguy@users.noreply.github.com>
2022-11-14 19:28:30 +0100

    Add files via upload

---
+++ c++/Simple calculator (school04).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> //basic library for input and output
+
+using namespace std;
+
+int a ,b; //definites some variables//'int' means 'integer', in Polish 'liczba całkowita'
+
+int main()
+{
+    cout << "Hi! My name is Computek!\nPlease write some numbers!\n"; //shows text in firts and second row
+    cin >> a; //requests an integer from user. that number will be stored as 'a'
+    cin >> b; //requests an integer from user. that number will be stored as 'b'
+    //WARNING! we could use just 'cin >> a >> b;'
+    cout << a << "+" << b << "=" << a + b; //shows text and variables and some calculations//we need to use '<<' to separate text from variables
+    return 0; //gives us '0' as a result
+}
