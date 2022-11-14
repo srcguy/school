480b74845000edfb2052c77db60953f4eb731769
Bruno S <81621691+srcguy@users.noreply.github.com>
2022-11-14 19:22:27 +0100

    Add files via upload

---
+++ c++/Basic calculations on variables (school03).cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream> //basic library for input and output
+
+using namespace std;
+
+int a = 5, b = 2; //definites some variables//'int' means 'integer', in Polish 'liczba całkowita'
+
+int main()
+{
+    cout << "Hi! My name is Computek!\nI will show you some calculation!\n"; //shows text in firts and second row
+    cout << a << "+" << b << "=" << a + b; //shows text and variables and some calculations//we need to use '<<' to separate text from variables
+    return 0; //gives us '0' as a result
+}
