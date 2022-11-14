e169aa4adc694ece44bae4965f6ef781d4e82715
Bruno S <81621691+srcguy@users.noreply.github.com>
2022-11-14 19:16:24 +0100

    Add files via upload

---
+++ c++/Adding new row of text (school02).cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream> //basic library for input and output
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\nOh God! My nuts!"; //shows text in firts and second row
+    return 0; //gives us '0' as a result
+}
