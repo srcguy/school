7facc428ad6f683bd34f2903be007630a42642cc
Bruno S <81621691+srcguy@users.noreply.github.com>
2022-11-16 17:44:03 +0100

    Add files via upload

---
+++ python/Functions (school11p).py
@@ -0,0 +1,5 @@
+def myfunction():#defines a function
+    print(x * x)#shows simple calculation
+
+x = int(input("Enter a number: "))#requests an integer from user. that number will be stored as 'x'
+myfunction()#runs a function
