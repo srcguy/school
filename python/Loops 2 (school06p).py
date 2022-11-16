b44de8567c0da0efe780314ae6f22c133429d05b
Bruno S <81621691+srcguy@users.noreply.github.com>
2022-11-16 16:58:07 +0100

    Update Loops 2 (school06p).py

--- python/Loops 2 (school06p).py
+++ python/Loops 2 (school06p).py
@@ -1,4 +1,4 @@
-n = int(input("How many times should i ask you for numbers?: "))
+n = int(input("How many times should i ask you for numbers?: "))#requests an integer from user.
 for i in range(n):#execute 'n' times
     x = int(input("Enter firts number: "))#requests an integer from user. that number will be stored as 'x'
     y = int(input("Enter second number: "))#requests an integer from user. that number will be stored as 'y'
