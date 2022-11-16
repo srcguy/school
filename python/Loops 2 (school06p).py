d3f9b3964ef74cb0238cb61e1e6633c48735475e
Bruno S <81621691+srcguy@users.noreply.github.com>
2022-11-16 16:57:55 +0100

    Update Loops 2 (school06p).py

--- python/Loops 2 (school06p).py
+++ python/Loops 2 (school06p).py
@@ -1,5 +1,5 @@
 n = int(input("How many times should i ask you for numbers?: "))
-for i in range(n):#execute five times
+for i in range(n):#execute 'n' times
     x = int(input("Enter firts number: "))#requests an integer from user. that number will be stored as 'x'
     y = int(input("Enter second number: "))#requests an integer from user. that number will be stored as 'y'
     print(x, "+", y, "=", x+y)#shows text and variables and some calculations#we need to use ',' to separate text from variables
