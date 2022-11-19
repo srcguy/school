22fe1a500f75c033bb010cf9e177b7df2adab4ae
Bruno S <81621691+srcguy@users.noreply.github.com>
2022-11-19 21:58:46 +0100

    Add files via upload

---
+++ c++/Functions (school06).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream> //basic library for input and output
+
+using namespace std;
+
+int n; //definites some variables//'int' means 'integer', in Polish 'liczba całkowita'
+
+int add(int a) //definites function 'add' with parameter 'a'
+{
+    return a + a; //return 'a + a'//in this case, 'n + n'
+}
+int main()
+{
+    cout << "Enter one number\n"; //shows text in firts row
+    cin >> n; //requests an integer from user. that number will be stored as 'n'
+    cout << add(n); //shows function 'add' that takes 'n' as a parameter 'a', so 'a' in function equals 'n'
+    return 0; //gives us '0' as a result
+}
