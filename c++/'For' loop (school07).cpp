4c696477c0be8312e0841c25f05bd10cbf468f59
Bruno S <81621691+srcguy@users.noreply.github.com>
2023-01-01 19:30:15 +0100

    Create 'For' loop (school07).cpp

---
+++ c++/'For' loop (school07).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i=1; i<=10; i++) //defines 'i', defines loop (if 'i' is smaller than 10 or equal 10 then...), defines what to do after one loop ('i++' means 'i=i+1')
+    {
+        cout << i << endl;;
+    }
+}
