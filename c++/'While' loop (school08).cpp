b9e60e89d30e8ba26ca784eeaea014366338ceb8
Bruno S <81621691+srcguy@users.noreply.github.com>
2023-01-04 21:15:19 +0100

    Create 'While' loop (school08).cpp

---
+++ c++/'While' loop (school08).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int a = 1; //defines variable 'a'
+
+int main()
+{
+    while (a <= 5) //while a is smaller or equal 5
+    {
+        cout << a << endl; //show 'a'
+        a++; //a=a+1
+    }
+}
